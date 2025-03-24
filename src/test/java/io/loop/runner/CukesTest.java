712838fd9f6e8ec50a5a07b2c4326c9f73d15bb1
Ketevan Karchava <ketevan0915@gmail.com>
2025-03-23 17:38:38 -0700

    6 scenarios, pass, updated pom to run mvn test

--- src/test/java/io/loop/runner/CukesTest.java
+++ src/test/java/io/loop/runner/CukesTest.java
@@ -6,15 +6,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {//"pretty",
+        plugin = {
+                //"pretty",
                 "html:target/html-reports/cucumber-report.html",
                 "json:target/cucumber.json",
                 "rerun:target/rerun.txt",
-                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+                "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" },
         features = "src/test/resources/feature",
         glue = "io/loop/step_definitions",
         dryRun = false,
-        tags = "@SmokeTest",
+        //tags = "@SmokeTest",
         monochrome = true,
         publish = false
 )
